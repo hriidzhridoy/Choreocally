6a2f8a174edc807e1988a1777b5d195563302370
hriidzhridoy <amamunhridoy@gmail.com>
2025-12-15 12:05:57 +0600

    Tiktokclone

---
+++ src/app/(tabs)/newPost.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function NewPostScreen() {
+  return (
+    <View>
+      <Text>NewPostScreen</Text>
+    </View>
+  );
+}
